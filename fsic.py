2016cfba3636e116183d6eee8f29a427249a3e1b
Chris Thoung <chris.thoung@gmail.com>
2020-02-11 21:41:13 +0000

    TYP: Add more type annotations

--- fsic.py
+++ fsic.py
@@ -11,10 +11,9 @@ __version__ = '0.3.0.dev'
 import copy
 import enum
 import itertools
-import numbers
+from numbers import Number
 import re
-from types import FunctionType
-from typing import Dict, List, NamedTuple, Optional, Union
+from typing import Any, Dict, Hashable, Iterator, List, Match, NamedTuple, Optional, Sequence, Union
 
 import numpy as np
 
@@ -113,6 +112,8 @@ class Term(NamedTuple):
 
         # If not a function, add the index
         if self.type != Type.FUNCTION:
+            assert self.index_ is not None
+
             if self.index_ > 0:
                 index = '[t+{}]'.format(self.index_)
             elif self.index_ == 0:
@@ -144,15 +145,15 @@ class Symbol(NamedTuple):
     """Container for information about a single symbol of an equation or model."""
     name: str
     type: Type
-    lags: Union[int, None]
-    leads: Union[int, None]
-    equation: Union[str, None]
-    code: Union[str, None]
+    lags: Optional[int]
+    leads: Optional[int]
+    equation: Optional[str]
+    code: Optional[str]
 
-    def combine(self, other):
+    def combine(self, other: 'Symbol') -> 'Symbol':
         """Combine the attributes of two symbols."""
 
-        def resolve_strings(old, new):
+        def resolve_strings(old: Optional[str], new: Optional[str]) -> Optional[str]:
             resolved = old
 
             if old is not None and new is not None:
@@ -195,10 +196,10 @@ class Symbol(NamedTuple):
 
 # Parser functions to convert strings to objects ------------------------------
 
-def split_equations_iter(model: str):
+def split_equations_iter(model: str) -> Iterator[str]:
     """Return the equations of `model` as an iterator of strings."""
 
-    def strip_comments(line: str):
+    def strip_comments(line: str) -> str:
         hash_position = line.find('#')
         if hash_position == -1:
             return line
@@ -229,15 +230,15 @@ def split_equations_iter(model: str):
                 yield equation
             buffer = []
 
-def split_equations(model: str):
+def split_equations(model: str) -> List[str]:
     """Return the equations of `model` as a list of strings."""
     return list(split_equations_iter(model))
 
 
-def parse_terms(expression: str):
+def parse_terms(expression: str) -> List[Term]:
     """Return the terms of `expression` as a list of Term objects."""
 
-    def process_term_match(match):
+    def process_term_match(match: Match[str]) -> Term:
         """Return the contents of `match` as a Term object."""
         groupdict = match.groupdict()
 
@@ -248,11 +249,13 @@ def parse_terms(expression: str):
         type_key = type_key_list[0]
 
         # Add implicit index (0) to non-function terms
-        index = groupdict['INDEX']
+        index: Optional[int] = None
+
+        index_ = groupdict['INDEX']
         if type_key != '_FUNCTION':
-            if index is None:
-                index = '0'
-            index = int(index)
+            if index_ is None:
+                index_ = '0'
+            index = int(index_)
 
         return Term(name=groupdict[type_key],
                     type=Type[type_key[1:]],
@@ -261,10 +264,10 @@ def parse_terms(expression: str):
     return [process_term_match(m)
             for m in term_re.finditer(expression)]
 
-def parse_equation_terms(equation: str):
+def parse_equation_terms(equation: str) -> List[Term]:
     """Return the terms of `equation` as a list of Term objects."""
 
-    def replace_type(term, new_type):
+    def replace_type(term: Term, new_type: Type) -> Term:
         if term.type == Type.VARIABLE:
             term = term._replace(type=new_type)
         return term
@@ -276,7 +279,7 @@ def parse_equation_terms(equation: str):
 
     return lhs_terms + rhs_terms
 
-def parse_equation(equation: str):
+def parse_equation(equation: str) -> List[Symbol]:
     """Return the symbols of `equation` as a list of Symbol objects."""
     terms = parse_equation_terms(equation)
 
@@ -287,11 +290,11 @@ def parse_equation(equation: str):
 
     # `symbols` stores the final symbols and is successively updated in the
     # loop below
-    symbols: Dict = {}
+    symbols: Dict[str, Symbol] = {}
 
     # `functions` keeps track of functions seen, to avoid duplicating entries
     # in `symbols`
-    functions: Dict = {}
+    functions: Dict[str, Symbol] = {}
 
     for term in terms:
         symbol = Symbol(name=term.name,
@@ -323,14 +326,14 @@ def parse_equation(equation: str):
 
     return list(symbols.values())
 
-def parse_model(model: str):
+def parse_model(model: str) -> List[Symbol]:
     """Return the symbols of `model` as a list of Symbol objects."""
     # Symbols: one list per model equation
     symbols_by_equation = list(map(parse_equation, split_equations_iter(model)))
 
     # Store combined symbols to a dictionary, successively combining in the
     # loop below
-    symbols: Dict = {}
+    symbols: Dict[str, Symbol] = {}
     for symbol in itertools.chain(*symbols_by_equation):
         name = symbol.name
         symbols[name] = symbols.get(name, symbol).combine(symbol)
@@ -343,21 +346,21 @@ def parse_model(model: str):
 class BaseModel:
     """Base class for economic models."""
 
-    ENDOGENOUS: List = []
-    EXOGENOUS: List = []
+    ENDOGENOUS: List[str] = []
+    EXOGENOUS: List[str] = []
 
-    PARAMETERS: List = []
-    ERRORS: List = []
+    PARAMETERS: List[str] = []
+    ERRORS: List[str] = []
 
-    NAMES = ENDOGENOUS + EXOGENOUS + PARAMETERS + ERRORS
-    CHECK = ENDOGENOUS
+    NAMES: List[str] = ENDOGENOUS + EXOGENOUS + PARAMETERS + ERRORS
+    CHECK: List[str] = ENDOGENOUS
 
-    LAGS = 0
-    LEADS = 0
+    LAGS: int = 0
+    LEADS: int = 0
 
-    CODE = None
+    CODE: Optional[str] = None
 
-    def __init__(self, span, *, dtype=float, default_value=0.0, **initial_values):
+    def __init__(self, span: Sequence[Hashable], *, dtype: Any = float, default_value: Union[int, float] = 0.0, **initial_values: Dict[str, Any]) -> None:
         """Initialise model variables.
 
         Parameters
@@ -396,7 +399,7 @@ class BaseModel:
                                      "(expect {} elements)".format(
                                          name, len(self.span)))
 
-    def __getattr__(self, name):
+    def __getattr__(self, name: str) -> Any:
         """Over-riding method to provide indirect access to internal model
         variables as needed."""
         if (name in self.__dict__['names'] or
@@ -405,12 +408,12 @@ class BaseModel:
         else:
             return super().__getattribute__(name)
 
-    def __setattr__(self, name, value):
+    def __setattr__(self, name: str, value: Any) -> None:
         """Over-riding method to provide indirect access to internal model
         variables as needed."""
         if (name in self.__dict__['names'] or
             name == 'iterations'):
-            if isinstance(value, numbers.Number):
+            if isinstance(value, Number):
                 self.__dict__['_' + name][:] = value
             elif len(self.__dict__['_' + name]) == len(value):
                 self.__dict__['_' + name] = np.array(
@@ -440,7 +443,7 @@ class BaseModel:
         else:
             super.__setattr__(self, name, value)
 
-    def __getitem__(self, key):
+    def __getitem__(self, key: str) -> Any:
         if isinstance(key, str):
             if key in self.names:
                 return self.__getattr__(key)
@@ -469,7 +472,7 @@ class BaseModel:
                 location = self.span.index(index)
                 return values[location]
 
-    def __setitem__(self, key, value):
+    def __setitem__(self, key: str, value: Any) -> None:
         if isinstance(key, str):
             self.__setattr__(key, value)
         else:
@@ -494,7 +497,7 @@ class BaseModel:
                 location = self.span.index(index)
                 self.__dict__['_' + name][location] = value
 
-    def copy(self):
+    def copy(self) -> 'BaseModel':
         """Return a copy of the current (state of the) model instance."""
         copied = self.__class__(span=copy.deepcopy(self.span))
         copied.__dict__.update(
@@ -505,23 +508,23 @@ class BaseModel:
     __copy__ = copy
     __deepcopy__ = copy
 
-    def __dir__(self):
+    def __dir__(self) -> List[str]:
         return sorted(
             dir(type(self)) +
             self.__dict__['names'] +
             ['span', 'names', 'status', 'iterations'])
 
-    def _ipython_key_completions_(self):
+    def _ipython_key_completions_(self) -> List[str]:
         return self.__dict__['names']
 
     @property
-    def values(self):
+    def values(self) -> np.ndarray:
         """Model variable values as a 2D (variables x time) array."""
         return np.array([
             self.__getattribute__('_' + name) for name in self.names
         ])
 
-    def solve(self, *, start=None, end=None, max_iter=100, tol=1e-10, **kwargs):
+    def solve(self, *, start: Optional[Hashable] = None, end: Optional[Hashable] = None, max_iter: int = 100, tol: Union[int, float] = 1e-10, **kwargs: Dict[str, Any]) -> None:
         """Solve the model. Use default periods if none provided.
 
         Parameters
@@ -553,7 +556,7 @@ class BaseModel:
         for t in range(start_t, end_t + 1):
             self.solve_t(t, max_iter=max_iter, tol=tol, **kwargs)
 
-    def solve_period(self, period, *, max_iter=100, tol=1e-10, **kwargs):
+    def solve_period(self, period: Hashable, *, max_iter: int = 100, tol: Union[int, float] = 1e-10, **kwargs: Dict[str, Any]) -> None:
         """Solve a single period.
 
         Parameters
@@ -570,7 +573,7 @@ class BaseModel:
         t = self.span.index(period)
         self.solve_t(t, max_iter=max_iter, tol=tol, **kwargs)
 
-    def solve_t(self, t, *, max_iter=100, tol=1e-10, **kwargs):
+    def solve_t(self, t: int, *, max_iter: int = 100, tol: Union[int, float] = 1e-10, **kwargs: Dict[str, Any]) -> None:
         """Solve for the period at integer position `t` in the model's `span`.
 
         Parameters
@@ -584,7 +587,7 @@ class BaseModel:
         kwargs :
             Further keyword arguments to pass to the solution routines
         """
-        def get_check_values():
+        def get_check_values() -> np.ndarray:
             """Return a 1D NumPy array of variable values for checking in the current period."""
             return np.array([
                 self.__dict__['_' + name][t] for name in self.CHECK
@@ -611,7 +614,7 @@ class BaseModel:
         self.status[t] = status
         self.iterations[t] = iteration
 
-    def _evaluate(self, t, **kwargs):
+    def _evaluate(self, t: int, **kwargs: Dict[str, Any]) -> None:
         """Evaluate the system of equations for the period at integer position `t` in the model's `span`.
 
         Parameters
@@ -642,7 +645,7 @@ class Model(BaseModel):
 {equations}\
 '''
 
-def build_model_definition(symbols: List):
+def build_model_definition(symbols: List[Symbol]) -> str:
     """Return a model class definition string from the contents of `symbols`."""
     # Separate variable names according to variable type
     endogenous = [s.name for s in symbols if s.type == Type.ENDOGENOUS]
@@ -674,7 +677,7 @@ def build_model_definition(symbols: List):
 
     return model_definition_string
 
-def build_model(symbols: List):
+def build_model(symbols: List[Symbol]) -> Any:
     """Return a model class definition from the contents of `symbols`. **Uses `exec()`.**"""
     model_definition_string = build_model_definition(symbols)
     exec(model_definition_string)
