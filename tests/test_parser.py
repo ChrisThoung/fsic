fa8396ed408d7c00953bd7c45b9179cb07118871
Chris Thoung <chris.thoung@gmail.com>
2023-09-16 18:53:10 +0100

    TST: Add further tests to check that `fsic`-generated model code solves

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -7,6 +7,8 @@ Test suite for fsic model/equation parser.
 
 import unittest
 
+import numpy as np
+
 import fsic
 
 
@@ -16,6 +18,8 @@ class TestParserPeriodIndexing(unittest.TestCase):
         # Check that the user can specify integer periods by enclosing them in
         # backticks
         test_input = 'Cb = C[`2000`]'
+
+        # Check symbols are generated correctly
         expected = [
             fsic.parser.Symbol(name='Cb',
                                type=fsic.parser.Type.ENDOGENOUS,
@@ -33,10 +37,31 @@ class TestParserPeriodIndexing(unittest.TestCase):
         symbols = fsic.parse_model(test_input)
         self.assertEqual(symbols, expected)
 
+        # Create a model class definition
+        Model = fsic.build_model(symbols)
+
+        # Solve the model
+        model = Model(range(2000, 2005 + 1))
+
+        model['C', 2000] = 10
+        model.solve(start=2001)
+
+        # Check results are as expected
+        self.assertTrue(np.allclose(
+            model.Cb,
+            np.array([0.0, 10.0, 10.0, 10.0, 10.0, 10.0])
+        ))
+        self.assertTrue(np.allclose(
+            model.C,
+            np.array([10.0, 0.0, 0.0, 0.0, 0.0, 0.0])
+        ))
+
     def test_period_index_verbatim_str_single_quote(self):
         # Check that the user can specify periods by enclosing them in
         # backticks (single quote string variant)
         test_input = "Cb = C[`'2000'`]"
+
+        # Check symbols are generated correctly
         expected = [
             fsic.parser.Symbol(name='Cb',
                                type=fsic.parser.Type.ENDOGENOUS,
@@ -54,10 +79,31 @@ class TestParserPeriodIndexing(unittest.TestCase):
         symbols = fsic.parse_model(test_input)
         self.assertEqual(symbols, expected)
 
+        # Create a model class definition
+        Model = fsic.build_model(symbols)
+
+        # Solve the model
+        model = Model(list(map(str, range(2000, 2005 + 1))))
+
+        model['C', '2000'] = 10
+        model.solve(start='2001')
+
+        # Check results are as expected
+        self.assertTrue(np.allclose(
+            model.Cb,
+            np.array([0.0, 10.0, 10.0, 10.0, 10.0, 10.0])
+        ))
+        self.assertTrue(np.allclose(
+            model.C,
+            np.array([10.0, 0.0, 0.0, 0.0, 0.0, 0.0])
+        ))
+
     def test_period_index_verbatim_str_double_quote(self):
         # Check that the user can specify periods by enclosing them in
         # backticks (double quote string variant)
         test_input = 'Cb = C[`"2000"`]'
+
+        # Check symbols are generated correctly
         expected = [
             fsic.parser.Symbol(name='Cb',
                                type=fsic.parser.Type.ENDOGENOUS,
@@ -75,6 +121,25 @@ class TestParserPeriodIndexing(unittest.TestCase):
 
         symbols = fsic.parse_model(test_input)
         self.assertEqual(symbols, expected)
+
+        # Create a model class definition
+        Model = fsic.build_model(symbols)
+
+        # Solve the model
+        model = Model(list(map(str, range(2000, 2005 + 1))))
+
+        model['C', '2000'] = 10
+        model.solve(start='2001')
+
+        # Check results are as expected
+        self.assertTrue(np.allclose(
+            model.Cb,
+            np.array([0.0, 10.0, 10.0, 10.0, 10.0, 10.0])
+        ))
+        self.assertTrue(np.allclose(
+            model.C,
+            np.array([10.0, 0.0, 0.0, 0.0, 0.0, 0.0])
+        ))
 
 
 if __name__ == '__main__':
