1f26495b0c0c3a02e5bbd013e6619bf451551bb4
Chris Thoung <chris.thoung@gmail.com>
2023-09-16 18:39:01 +0100

    TST: Add further tests for handling of verbatim code with strings

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -33,6 +33,49 @@ class TestParserPeriodIndexing(unittest.TestCase):
         symbols = fsic.parse_model(test_input)
         self.assertEqual(symbols, expected)
 
+    def test_period_index_verbatim_str_single_quote(self):
+        # Check that the user can specify periods by enclosing them in
+        # backticks (single quote string variant)
+        test_input = "Cb = C[`'2000'`]"
+        expected = [
+            fsic.parser.Symbol(name='Cb',
+                               type=fsic.parser.Type.ENDOGENOUS,
+                               lags=0,
+                               leads=0,
+                               equation="Cb[t] = C['2000']", code="self._Cb[t] = self['C', '2000']"),
+            fsic.parser.Symbol(name='C',
+                               type=fsic.parser.Type.EXOGENOUS,
+                               lags=0,
+                               leads=0,
+                               equation=None,
+                               code=None)
+        ]
+
+        symbols = fsic.parse_model(test_input)
+        self.assertEqual(symbols, expected)
+
+    def test_period_index_verbatim_str_double_quote(self):
+        # Check that the user can specify periods by enclosing them in
+        # backticks (double quote string variant)
+        test_input = 'Cb = C[`"2000"`]'
+        expected = [
+            fsic.parser.Symbol(name='Cb',
+                               type=fsic.parser.Type.ENDOGENOUS,
+                               lags=0,
+                               leads=0,
+                               # TODO: Standardise on either single or double quotes?
+                               equation='Cb[t] = C["2000"]', code="self._Cb[t] = self['C', \"2000\"]"),
+            fsic.parser.Symbol(name='C',
+                               type=fsic.parser.Type.EXOGENOUS,
+                               lags=0,
+                               leads=0,
+                               equation=None,
+                               code=None)
+        ]
+
+        symbols = fsic.parse_model(test_input)
+        self.assertEqual(symbols, expected)
+
 
 if __name__ == '__main__':
     unittest.main()
